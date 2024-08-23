63042efcc405aa7385bb27a4febf546ba4e89231
GuilhermeJWT <guiromanno@gmail.com>
2024-08-23 16:32:44 -0300

    ADD: Projeto Financeiro Service

---
+++ financeiroservice/src/main/java/br/com/systemsgs/financeiroservice/FinanceiroserviceApplication.java
@@ -0,0 +1,12 @@
+package br.com.systemsgs.financeiroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroserviceApplication.class, args);
+	}
+}
---
+++ financeiroservice/src/test/java/br/com/systemsgs/financeiroservice/FinanceiroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.financeiroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
