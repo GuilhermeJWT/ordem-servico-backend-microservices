76fe047dd565467990baf385812348fea1347d5d
GuilhermeJWT <guiromanno@gmail.com>
2024-08-23 16:53:54 -0300

    ADD: Enums

---
+++ financeiroservice/src/main/java/br/com/systemsgs/financeiroservice/enums/StatusContas.java
@@ -0,0 +1,25 @@
+package br.com.systemsgs.financeiroservice.enums;
+
+public enum StatusContas {
+
+    EM_ABERTO("Em aberto"),
+    PENDENTE("Pendente"),
+    CONFIRMADA("Confirmada"),
+    BAIXADA("Baixada"),
+    ATRASADA("Atrasada"),
+    PAUSADA("Pausada"),
+    CANCELADA("Cancelada"),
+    VENCIDA("Vencida"),
+    INADIMPLENTE("Inadimplente"),
+    AGUARDANDO_REEMBOLSO("Aguardando Reembolso");
+
+    private final String descricao;
+
+    StatusContas(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
