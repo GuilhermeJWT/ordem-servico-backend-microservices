7bd7a4dd66ce397100229a8001879c22358da45f
GuilhermeJWT <guiromanno@gmail.com>
2024-08-23 10:50:11 -0300

    ADD: Projeto Cadastros Services

---
+++ cadastrosservice/src/main/java/br/com/systemsgs/cadastrosservice/CadastrosserviceApplication.java
@@ -0,0 +1,12 @@
+package br.com.systemsgs.cadastrosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosserviceApplication.class, args);
+	}
+}
---
+++ cadastrosservice/src/test/java/br/com/systemsgs/cadastrosservice/CadastrosserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.cadastrosservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
