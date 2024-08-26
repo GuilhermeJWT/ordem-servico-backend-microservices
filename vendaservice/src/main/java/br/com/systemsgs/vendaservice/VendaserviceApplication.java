432bb0f5c64c48ae1cfb6e2c70e53f205477a3fd
GuilhermeJWT <guiromanno@gmail.com>
2024-08-26 16:47:24 -0300

    ADD: Projeto Vendas Service

---
+++ vendaservice/src/main/java/br/com/systemsgs/vendaservice/VendaserviceApplication.java
@@ -0,0 +1,12 @@
+package br.com.systemsgs.vendaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaserviceApplication.class, args);
+	}
+}
