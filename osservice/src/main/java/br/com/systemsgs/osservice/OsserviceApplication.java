715c2a8db7bababfd494258c29a3ca28e61c8740
GuilhermeJWT <guiromanno@gmail.com>
2024-08-26 14:19:29 -0300

    ADD: Projeto OS Service

---
+++ osservice/src/main/java/br/com/systemsgs/osservice/OsserviceApplication.java
@@ -0,0 +1,12 @@
+package br.com.systemsgs.osservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsserviceApplication.class, args);
+	}
+}
---
+++ osservice/src/test/java/br/com/systemsgs/osservice/OsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.osservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
