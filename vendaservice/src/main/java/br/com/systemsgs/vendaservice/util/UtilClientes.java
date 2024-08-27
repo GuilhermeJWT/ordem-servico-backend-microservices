d65aace0eafad4acfdab968b87ddcf4aa50966dd
GuilhermeJWT <guiromanno@gmail.com>
2024-08-27 17:55:18 -0300

    ADD: Messages Properties - Vendas Service

--- vendaservice/src/main/java/br/com/systemsgs/vendaservice/util/UtilClientes.java
+++ vendaservice/src/main/java/br/com/systemsgs/vendaservice/util/UtilClientes.java
@@ -6,8 +6,6 @@ import br.com.systemsgs.vendaservice.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 public class UtilClientes {
 
