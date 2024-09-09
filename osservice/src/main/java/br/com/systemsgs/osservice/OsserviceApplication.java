f9650bb3938c2ff0d3d95f8aaddb8a1ccd535563
GuilhermeJWT <guiromanno@gmail.com>
2024-09-08 23:51:43 -0300

    ADD: Configurações do Kafka - Consumer

--- osservice/src/main/java/br/com/systemsgs/osservice/OsserviceApplication.java
+++ osservice/src/main/java/br/com/systemsgs/osservice/OsserviceApplication.java
@@ -2,7 +2,9 @@ package br.com.systemsgs.osservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class OsserviceApplication {
 
